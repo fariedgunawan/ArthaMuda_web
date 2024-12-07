c2642bb9ce18aaa5865dc76c8686a80362c30318
Muhammad Faried Gunawan <124080208+fariedgunawan@users.noreply.github.com>
2024-12-07 18:00:01 +0700

    Update tailwind.config.js

--- ArthaMuda/tailwind.config.js
+++ ArthaMuda/tailwind.config.js
@@ -1,5 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 const { nextui } = require("@nextui-org/react");
+/** @type {import('tailwindcss').Config} */
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
