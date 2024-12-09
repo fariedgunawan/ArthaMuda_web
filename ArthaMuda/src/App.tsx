80d457d94983d22975f197f02d91bf6e01e25f39
Muhammad Faried Gunawan <124080208+fariedgunawan@users.noreply.github.com>
2024-12-09 11:46:50 +0700

    add sebagian

--- ArthaMuda/src/App.tsx
+++ ArthaMuda/src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import AddIncome from "./pages/AddIncome";
+import AddOutcome from "./pages/AddOutcome";
+import ListTransaction from "./pages/ListTransaction";
 function App() {
   return (
     <Router>
@@ -9,6 +12,9 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/Register" element={<Register />} />
         <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/AddIncome" element={<AddIncome />} />
+        <Route path="/AddOutcome" element={<AddOutcome />} />
+        <Route path="/ListTransaction" element={<ListTransaction />} />
       </Routes>
     </Router>
   );
--- ArthaMuda/src/pages/AddOutcome.tsx
+++ ArthaMuda/src/pages/AddOutcome.tsx
@@ -1,55 +1,44 @@
 import { FaUserCircle } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
 import background from "../assets/bg-img.png";
-import income from "../assets/income.png";
 import outcome from "../assets/outcome.png";
-import stats from "../assets/stats.png";
-const Dashboard = () => {
-  const navigate = useNavigate();
+const AddOutcome = () => {
   return (
     <div className="bg-cover bg-no-repeat min-h-screen" style={{ backgroundImage: `url(${background})` }}>
       <div className="body flex flex-col">
-        <div className="head bg-[#3339B4] rounded-b-[30px] pb-[80px] px-[16px] pt-[30px]">
+        <div className="head bg-[#3339B4] flex flex-col rounded-b-[30px] pb-[20px] px-[16px] pt-[30px]">
           <div className="hello flex flex-row items-center justify-between text-white">
-            <h2 className="text-[25px] font-semibold">Hello User..</h2>
+            <h2 className="text-[25px] font-semibold">Add Outcome</h2>
             <FaUserCircle className="text-[30px]" />
           </div>
-          <div className="balance flex flex-col text-white mt-[40px]">
-            <h2 className="font-medium text-[22px]">You have</h2>
-            <div className="balance mt-[10px] flex flex-row items-center justify-between">
-              <h2 className="text-[25px] font-extrabold">Rp. 16.000.000</h2>
-              <h2>See Details</h2>
+          <div className="input flex flex-col gap-[30px] text-white mt-[40px]">
+            <div className="input-caption flex flex-row items-center gap-[10px]">
+              <h2 className="text-[20px]">Title</h2>
+              <input type="text" placeholder="Input Title Here.." className=" text-[16px] bg-transparent outline-none border-b-2 w-full" />
             </div>
-            <h2 className="text-[16px] mt-[10px]">Last Update 10.00 12/05/24</h2>
+            <div className="input-caption flex flex-row items-center gap-[10px]">
+              <h2 className="text-[20px]">Rp.</h2>
+              <input type="number" placeholder="Input Balance Here.." className=" text-[16px] bg-transparent outline-none border-b-2 w-full" />
+            </div>
+            <div className="input-caption flex flex-row items-center gap-[10px]">
+              <h2 className="text-[20px]">Date</h2>
+              <input type="date" placeholder="Input Balance Here.." className=" text-[16px] bg-transparent outline-none border-b-2 w-full" />
+            </div>
           </div>
-        </div>
-        <div className="card-navigator flex flex-row items-center -mt-[60px] justify-center gap-[50px] mx-[30px] rounded-2xl py-[8px] bg-white">
-          <div className="card-1 flex flex-col items-center" onClick={() => navigate("/AddIncome")}>
-            <img src={income} className="w-[40px]" alt="" />
-            <h2 className="mt-[10px] font-medium text-[#3339B4]">Income</h2>
-          </div>
-          <div className="card-1 flex flex-col items-center" onClick={() => navigate("/AddOutcome")}>
-            <img src={outcome} className="w-[40px]" alt="" />
-            <h2 className="mt-[12px] font-medium text-[#3339B4]">Outcome</h2>
-          </div>
-          <div className="card-1 flex flex-col items-center">
-            <img src={stats} className="w-[40px]" alt="" />
-            <h2 className="mt-[12px] font-medium text-[#3339B4]">Stats</h2>
-          </div>
+          <button className="flex justify-end text-white font-semibold text-[20px] mt-[20px]">Add +</button>
         </div>
 
         <div className="list-data mt-[30px] flex flex-col px-[16px]">
           <div className="head-data flex flex-row items-center justify-between">
-            <h2 className="font-bold text-[20px] text-[#3339B4]">Recent</h2>
+            <h2 className="font-bold text-[20px] text-[#3339B4]">Last Outcome</h2>
             <h2 className="font-medium text-[16px] text-[#3339B4]">See More</h2>
           </div>
           <div className="container-card-list flex flex-col gap-[10px] mt-[20px]">
             <div className="card-1 bg-[#3339B4] flex flex-row justify-between items-center px-[16px] py-[15px] rounded-2xl">
               <div className="right-side flex flex-row items-center gap-[20px]">
-                <img src={income} className="w-[50px]" alt="" />
+                <img src={outcome} className="w-[50px]" alt="" />
                 <div className="caption-data flex flex-col gap-[5px] items-start">
-                  <h2 className="text-white font-semibold text-[18px]">Uang Jajan</h2>
-                  <h2 className="text-[#11FFC6]">+ Rp. 500.000,00</h2>
+                  <h2 className="text-white font-semibold text-[18px]">Somay</h2>
+                  <h2 className="text-[#FF4343]">- Rp. 10.000,00</h2>
                 </div>
               </div>
               <div className="left-side flex flex-col items-end text-white text-[12px]">
@@ -61,7 +50,7 @@ const Dashboard = () => {
               <div className="right-side flex flex-row items-center gap-[20px]">
                 <img src={outcome} className="w-[50px]" alt="" />
                 <div className="caption-data flex flex-col gap-[5px] items-start">
-                  <h2 className="text-white font-semibold text-[18px]">Martabak</h2>
+                  <h2 className="text-white font-semibold text-[18px]">Bubur</h2>
                   <h2 className="text-[#FF4343]">- Rp. 20.000,00</h2>
                 </div>
               </div>
@@ -74,7 +63,7 @@ const Dashboard = () => {
               <div className="right-side flex flex-row items-center gap-[20px]">
                 <img src={outcome} className="w-[50px]" alt="" />
                 <div className="caption-data flex flex-col gap-[5px] items-start">
-                  <h2 className="text-white font-semibold text-[18px]">PDAM</h2>
+                  <h2 className="text-white font-semibold text-[18px]">Iuran RW</h2>
                   <h2 className="text-[#FF4343]">- Rp. 100.000,00</h2>
                 </div>
               </div>
@@ -85,10 +74,10 @@ const Dashboard = () => {
             </div>
             <div className="card-1 bg-[#3339B4] flex flex-row justify-between items-center px-[16px] py-[15px] rounded-2xl">
               <div className="right-side flex flex-row items-center gap-[20px]">
-                <img src={income} className="w-[50px]" alt="" />
+                <img src={outcome} className="w-[50px]" alt="" />
                 <div className="caption-data flex flex-col gap-[5px] items-start">
-                  <h2 className="text-white font-semibold text-[18px]">Uang Jajan</h2>
-                  <h2 className="text-[#11FFC6]">+ Rp. 500.000,00</h2>
+                  <h2 className="text-white font-semibold text-[18px]">Jajan Adek</h2>
+                  <h2 className="text-[#FF4343]">- Rp. 50.000,00</h2>
                 </div>
               </div>
               <div className="left-side flex flex-col items-end text-white text-[12px]">
@@ -103,4 +92,4 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+export default AddOutcome;
